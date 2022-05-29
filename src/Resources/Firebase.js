a81b05500e3a37e807d628564fdef803dfab7bff
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-05-29 12:36:43 +0530

    feat: installed firebase

---
+++ src/Resources/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9JPUnXLoDRTa8AH1A-_99xu9SaxurcBo",
+  authDomain: "hotflix-d23ce.firebaseapp.com",
+  projectId: "hotflix-d23ce",
+  storageBucket: "hotflix-d23ce.appspot.com",
+  messagingSenderId: "781521976551",
+  appId: "1:781521976551:web:eedb6c75f276c373ceb642"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
