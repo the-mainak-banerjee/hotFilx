3dff958b951f880baa8b008dd21c2be42e5ca965
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-05-31 09:04:13 +0530

    style- added style on active link of Navbar

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path='/' element={<Navigate to='/home'/>}/>
+          <Route path='/' element={<Navigate replace to='/home'/>}/>
           <Route path='/home' element={<Home/>}/>
           <Route path='/movies' element={<Movies />}/>
           <Route path="/tv" element={<TvShows />} />
           <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/login' element={user?.email ? <Navigate to='/home'/> : <LogIn/>}/>
-          <Route path='/account' element={user?.email ? <Account /> : <Navigate to='/login'/>}/>
+          <Route path='/login' element={user?.email ? <Navigate replace to='/home'/> : <LogIn/>}/>
+          <Route path='/account' element={user?.email ? <Account /> : <Navigate replace to='/login'/>}/>
         </Routes>
     </>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { AiOutlineSearch, AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 import { MdPersonalVideo, MdOutlineMovie } from 'react-icons/md'
 import { useAuth } from '../Store/auth-context'
@@ -28,28 +28,28 @@ function Navbar() {
                     <input type="search" placeholder='Search' autoComplete="off" className="bg-inherit border-0 border-b-2 pl-1 focus:w-[250px]"/>
                     <AiOutlineSearch className="absolute top-7 right-[345px]"/>
                 </div>
-                <Link to='/tv' className="mr-4 hover:underline hover:text-white">
+                <NavLink to='/tv' className={(navData) => navData.isActive ? 'underline text-white nav-link' : 'nav-link'}>
                     <h4>TV</h4>
-                </Link>
-                <Link to='/movies' className="mr-4 hover:underline hover:text-white">
+                </NavLink>
+                <NavLink to='/movies' className={(navData) => navData.isActive ? 'underline text-white nav-link' : 'nav-link'}>
                     <h4>Movies</h4>
-                </Link>
+                </NavLink>
                 {user?.email ? (
                     <div className='flex items-center'>
-                        <Link to='/account' className="mr-4 hover:underline hover:text-white">
+                        <NavLink to='/account' className={(navData) => navData.isActive ? 'underline text-white nav-link' : 'nav-link'}>
                             <h4>Account</h4>
-                        </Link>                
-                            <button onClick={handleLogOut} className='bg-[#1f80e0] px-5 py-1 rounded text-white font-bold hover:bg-white hover:text-black'>LogOut</button>
+                        </NavLink>                
+                            <button onClick={handleLogOut} className='nav-btn'>LogOut</button>
                     </div>
                 ) : 
                 (
                     <div className='flex items-center'>
-                        <Link to='/login' className="mr-4 hover:underline hover:text-white">
+                        <NavLink to='/login' className={(navData) => navData.isActive ? 'underline text-white nav-link' : 'nav-link'}>
                             <h4>Login</h4>
-                        </Link>                
-                        <Link to='/signup' className="mr-4">
-                            <button className='bg-[#1f80e0] px-5 py-1 rounded text-white font-bold hover:bg-white hover:text-black'>SignUp</button>
-                        </Link>
+                        </NavLink>                
+                        <NavLink to='/signup' className="mr-4">
+                            <button className='nav-btn'>SignUp</button>
+                        </NavLink>
                     </div>
                 )
                 }
@@ -61,35 +61,39 @@ function Navbar() {
                 <div className='flex items-center'> 
                     <h2 className="font-semibold text-2xl mr-5">HotFlix</h2>
                     {user?.email ? 
-                    <button onClick={handleLogOut} className='bg-[#1f80e0] px-5 py-1 rounded text-white font-bold mr-4'>LogOut</button> :
-                    <Link to='/signup' className="mr-4">
-                        <button className='bg-[#1f80e0] px-5 py-1 rounded text-white font-bold '>SignUp</button>
-                    </Link>}
+                    <button onClick={handleLogOut} className='bg-[#1f80e0] px-5 py-1 rounded text-white font-bold mr-4'>
+                        LogOut
+                    </button> :
+                    <NavLink to='/signup' className="mr-4">
+                        <button className='bg-[#1f80e0] px-5 py-1 rounded text-white font-bold '>
+                            SignUp
+                        </button>
+                    </NavLink>}
                 </div>
                 <AiOutlineSearch />
             </div>
             <div className='bg-[#0C111B] text-white flex items-center justify-center z-50 pt-1 fixed bottom-[1px] w-[100%]'>
-                <Link to='/home' className="mr-12 flex-col">
+                <NavLink to='/home' className="mr-12 flex-col">
                     <AiOutlineHome className='ml-[13px]'/>
                     <h4>Home</h4>
-                </Link>
-                <Link to='/tv' className="mr-12 flex-col">
+                </NavLink>
+                <NavLink to='/tv' className="mr-12 flex-col">
                     <MdPersonalVideo className='ml-[2px]'/>
                     <h4>TV</h4>
-                </Link>
-                <Link to='/movies' className="mr-12 flex-col">
+                </NavLink>
+                <NavLink to='/movies' className="mr-12 flex-col">
                     <MdOutlineMovie className='ml-[15px]'/>
                     <h4>Movies</h4>
-                </Link>
+                </NavLink>
                 {user?.email ?
-                    <Link to='/account' className="flex-col">
+                    <NavLink to='/account' className="flex-col">
                     <AiOutlineUser className='ml-[20px]'/>
                     <h4>Account</h4>
-                </Link> : 
-                <Link to='/login' className="flex-col">
+                </NavLink> : 
+                <NavLink to='/login' className="flex-col">
                     <AiOutlineUser className='ml-[12px]'/>
                     <h4>LogIn</h4>
-                </Link>}
+                </NavLink>}
             </div>
         </nav>
     </>
