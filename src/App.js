399c1ebac64d0dc0d04dff270e94085687d62fb7
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-06-01 20:49:54 +0530

    Added Lazy Loading and Restructured The Folders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React , { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Account from "./Pages/Account/Account";
-import ChangePassword from "./Pages/Account/ChangePassword";
-import Home from './Pages/Home'
+
+import Home from './Pages/Main/Home'
+import Movies from './Pages/Main/Movies'
+import TvShows from "./Pages/Main/TvShows";
 import LogIn from "./Pages/User/LogIn";
 import SignUp from "./Pages/User/SignUp";
-import ForgotPassword from "./Pages/User/ForgotPassword";
-import Movies from './Pages/Movies'
-import Trailer from "./Pages/Trailer";
-import TvShows from "./Pages/TvShows";
+// import Trailer from "./Pages/Main/Trailer";
+// import Account from "./Pages/Account/Account";
+// import ChangePassword from "./Pages/Account/ChangePassword";
+// import DeleteAccount from "./Pages/Account/DeleteAccount";
+// import ForgotPassword from "./Pages/User/ForgotPassword";
+
 import { useAuth } from "./Store/auth-context";
-import DeleteAccount from "./Pages/Account/DeleteAccount";
+import Loading from './Components/UI/Loading'
+
+const Trailer = React.lazy(() => import('./Pages/Main/Trailer'))
+const Account = React.lazy(() => import('./Pages/Account/Account'))
+const ChangePassword = React.lazy (() => import('./Pages/Account/ChangePassword'))
+const ForgotPassword = React.lazy(() => import('./Pages/User/ForgotPassword'))
+const DeleteAccount = React.lazy(() => import('./Pages/Account/DeleteAccount'))
 
 function App() {
 
@@ -17,19 +27,25 @@ function App() {
 
   return (
     <>
-        <Routes>
-          <Route path='/' element={<Navigate replace to='/home'/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/movies' element={<Movies />}/>
-          <Route path="/tv" element={<TvShows />} />
-          <Route path='home/:showTitle/:showId' element={<Trailer />}/>
-          <Route path='/signup' element={user?.email ? <Navigate replace to='home'/> : <SignUp/>}/>
-          <Route path='/login' element={user?.email ? <Navigate replace to='/home'/> : <LogIn/>}/>
-          <Route path='/forgot_password' element={user?.email ? <Navigate replace to='/home'/> : <ForgotPassword/>}/>
-          <Route path='/account' element={user?.email ? <Account /> : <Navigate replace to='/login'/>}/>
-          <Route path='/account/change_password' element={user?.email ? <ChangePassword /> : <Navigate replace to='/login'/>}/>
-          <Route path='/account/delete_account' element={user?.email ? <DeleteAccount /> : <Navigate replace to='/login'/>}/>
-        </Routes>
+        <Suspense 
+          fallback={
+            <Loading/>
+          }  
+        >
+          <Routes>
+            <Route path='/' element={<Navigate replace to='/home'/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/movies' element={<Movies />}/>
+            <Route path="/tv" element={<TvShows />} />
+            <Route path='home/:showTitle/:showId' element={<Trailer />}/>
+            <Route path='/signup' element={user?.email ? <Navigate replace to='home'/> : <SignUp/>}/>
+            <Route path='/login' element={user?.email ? <Navigate replace to='/home'/> : <LogIn/>}/>
+            <Route path='/forgot_password' element={user?.email ? <Navigate replace to='/home'/> : <ForgotPassword/>}/>
+            <Route path='/account' element={user?.email ? <Account /> : <Navigate replace to='/login'/>}/>
+            <Route path='/account/change_password' element={user?.email ? <ChangePassword /> : <Navigate replace to='/login'/>}/>
+            <Route path='/account/delete_account' element={user?.email ? <DeleteAccount /> : <Navigate replace to='/login'/>}/>
+          </Routes>
+        </Suspense>
     </>
   );
 }
